8540e567c440cc58d6f162aa3a9e9c44458b2453
KellyMark <kmark602@gmail.com>
2023-12-05 14:49:49 -0500

    add pick x or y

--- client/src/scenes/TicTacToe.js
+++ client/src/scenes/TicTacToe.js
@@ -14,9 +14,32 @@ class TicTacToe extends Phaser.Scene {
   create() {
     // Basic scene setup, like creating sprites
     // this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
-    this.graphics = this.add.graphics({
-      lineStyle: { width: 4, color: 0x000000 },
-    });
+    this.graphics = this.add.graphics({lineStyle : {width: 4, color: 0x000000}})
+
+    this.hasPicked = false;
+    this.playerPick = null;
+    this.pickText = this.add.text(280, 200, 'Which do you pick?', {fill: '#000000', fontSize: 26})
+    this.pickX = this.add.image(320, 280, 'x')
+    this.pickO = this.add.image(400, 280, 'o')
+    this.pickX.scale *= 0.2
+    this.pickX.setInteractive();
+    this.pickO.setInteractive();
+    this.pickX.on('pointerup', () => {
+      this.playerPick = 'x'
+      this.pickX.destroy();
+      this.pickO.destroy();
+      this.pickText.destroy();
+      this.hasPicked = true;
+
+    })
+    this.pickO.on('pointerup', () => {
+      this.playerPick = 'o';
+      this.pickX.destroy()
+      this.pickO.destroy();
+      this.pickText.destroy();
+      this.hasPicked = true;
+    })
+    this.player = null;
 
     let cellSize = 110;
     let rows = 3;
@@ -36,6 +59,7 @@ class TicTacToe extends Phaser.Scene {
     this.grid.cellSize = cellSize;
     this.grid.rows = rows;
     this.grid.cols = cols;
+    
 
     this.grid.lineY1 = {
       startX: cellSize + this.offsetX,
@@ -79,7 +103,7 @@ class TicTacToe extends Phaser.Scene {
     this.grid.drawLineDown = (line) => {
       if (line) {
         if (line.drawing) {
-          line.currentY += 7;
+          line.currentY += 10;
         }
         if (line.currentY >= line.endY) {
           line.currentY = line.endY;
@@ -111,24 +135,30 @@ class TicTacToe extends Phaser.Scene {
       }
     };
 
-    const createX = (location) => {
+    this.grid.topLeft = new Geom.Rectangle(this.offsetX, this.offsetY, cellSize, cellSize)
+    this.grid.topLeft.on('pointerup', () => {
+      console.log('top left')
+    })
+    // this.grid.topLeft.setInteractive();
+    // this.grid.topLeft.on('pointerup', () => {
+    //   console.log('top left')
+    // })
+
+    // this.playerMove(location) {
       
-    }
-    const createO = (location) => {
-      
-    }
+    // }
   
-    this.x = this.add.image(200, 200, 'x')
-    this.x.scale *= 0.2;
-    this.halfX = this.grid.cellSize / 2;
-    this.halfY = this.grid.cellSize / 2;
-    console.log(this.grid.cellSize + this.offsetX -this.halfX);
-    this.x.setPosition((this.grid.cellSize * 2) - this.halfX + this.offsetX, this.grid.cellSize + this.halfY + this.offsetY)
+    // this.x = this.add.image(200, 200, 'x')
+    // this.x.scale *= 0.2;
+    // this.halfX = this.grid.cellSize / 2;
+    // this.halfY = this.grid.cellSize / 2;
+    // console.log(this.grid.cellSize + this.offsetX -this.halfX);
+    // this.x.setPosition((this.grid.cellSize * 2) - this.halfX + this.offsetX, this.grid.cellSize + this.halfY + this.offsetY)
 
 
-    this.o = this.add.image(200, 200, 'o');
-    this.o.scale *= 1.3;
-    this.o.setPosition(this.grid.cellSize - this.halfX + this.offsetX, this.grid.cellSize + this.halfY + this.offsetY)
+    // this.o = this.add.image(200, 200, 'o');
+    // this.o.scale *= 1.3;
+    // this.o.setPosition(this.grid.cellSize - this.halfX + this.offsetX, this.grid.cellSize + this.halfY + this.offsetY)
     // this.grid.leftX = this.grid.cellSize + this.grid.halfX;
     // this.grid.topX = this.grid.cellSize + this.grid.halfX;
     // this.grid.leftO = this.grid.cellSize + this.grid.radius;
@@ -137,7 +167,7 @@ class TicTacToe extends Phaser.Scene {
 
   update() {
     // Game loop - logic that runs every frame
-    if (!this.grid.animComplete) {
+    if (!this.grid.animComplete && this.hasPicked) {
       if (this.grid.lineY1.drawing) {
         this.grid.drawLineDown(this.grid.lineY1);
 
@@ -163,11 +193,6 @@ class TicTacToe extends Phaser.Scene {
       if (this.grid.lineX2.drawing) {
         this.grid.drawLineAcross(this.grid.lineX2);
       }
-    } else {
-      this.grid.lineY1.drawing = false;
-      this.grid.lineY2.drawing = false;
-      this.grid.lineX1.drawing = false;
-      this.grid.lineX2.drawing = false;
     }
   }
 }
